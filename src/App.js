19e89528153cecb554faaf18a81d2732d6cd5150
jarrodskahill <jarrodskahill@gmail.com>
2022-02-22 23:46:57 -0800

    added social media logos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./styles/app.scss";
 import logo from "./logo.png";
+import twitter from "./imgs/twitter.png";
+import youtube from "./imgs/you.png";
+import insta from "./imgs/insta.png";
+import tik from "./imgs/tik2.png";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
         <img className="logo" src={logo} />
-        <div>
-          <a href="https://twitter.com/SkahillJarrod">Twitter</a>
-          <a></a>
+        <div className="socials">
+          <a href="https://twitter.com/SkahillJarrod">
+            <img src={twitter} />
+          </a>
+          <a href="https://www.youtube.com/channel/UCLpmWRfga-0AHqp-fHYDdDg">
+            <img src={youtube} />
+          </a>
+          <a href="">
+            <img src={insta} />
+          </a>
+          <a>
+            <img src={tik}></img>
+          </a>
         </div>
       </div>
     </div>
